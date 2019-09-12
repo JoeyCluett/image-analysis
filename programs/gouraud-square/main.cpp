50bd578eb9270c4b7b2ab44438d8fb1db53cf80a
JoeyCluett <joesunglassescool@gmail.com>
2019-09-12 12:18:22 -0500

    fixed regular gouraud shader program

--- programs/gouraud-square/main.cpp
+++ programs/gouraud-square/main.cpp
@@ -38,36 +38,8 @@ int main(int argc, char* argv[]) {
     
     // a bunch of stuff that is modified by the event loop
 
-    xy_pair current_point = {0.0, 0.0};
-    int mouse_x, mouse_y;
     bool running     = true;
     sdl_event_map_t sdl_event_map;
-
-    // capture the mouse position
-    sdl_event_map.insert({ 
-        SDL_MOUSEMOTION,
-        [
-                &current_point, 
-                &mouse_x, 
-                &mouse_y
-        ](void* ptr) {
-
-            auto* mouse_motion = reinterpret_cast<SDL_MouseMotionEvent*>(ptr);
-
-            //cout 
-            //    << "mouse motion callback [" 
-            //    << mouse_motion->x << " : " 
-            //    << mouse_motion->y << "]\n";
-
-            // calculate the mapped coordinates
-            float_t x = float_t(mouse_motion->x) / SCREEN_SIZE;
-            float_t y = float_t(mouse_motion->y) / SCREEN_SIZE;
-            mouse_x = mouse_motion->x;
-            mouse_y = mouse_motion->y;
-
-            current_point = { x, y }; 
-        }
-    });
 
     // capture quit condition
     sdl_event_map.insert({ 
@@ -84,46 +56,52 @@ int main(int argc, char* argv[]) {
 
     while(running) {
         sdl_evaluate_events(sdl_event_map);
-        auto slope = current_point.y / current_point.x;        
 
-        float_t final_intensity;
+        SDL_FillRect(s, NULL, 0); // clear screen
 
-        if(slope <= 1.0) {
-        
-            final_intensity = 2.0 * get_color(
-                current_point.x, current_point.y,
-                p[0], p[1], p[2],
-                c[0], c[1], c[2]
-            );
-        
+        for(int j = 0; j < SCREEN_SIZE; j++) {
+            for(int i = 0; i < SCREEN_SIZE; i++) {
+
+                auto slope = float_t(j) / float_t(i);
+                float_t mapped_x = float_t(i) / SCREEN_SIZE; 
+                float_t mapped_y = float_t(j) / SCREEN_SIZE;
+
+                float_t final_intensity;
+
+                if(slope <= 1.0) {
+                
+                    final_intensity = 2.0 * get_color(
+                        mapped_x, mapped_y,
+                        p[0], p[1], p[2],
+                        c[0], c[1], c[2]
+                    );
+                
+                }
+                else {
+                    
+                    final_intensity = 2.0 * get_color(
+                        mapped_x, mapped_y,
+                        p[0], p[2], p[3],
+                        c[0], c[2], c[3]
+                    );
+
+                }
+
+                //cout << "final intensity: " << final_intensity << endl;
+
+                int rgbv = (final_intensity * 255);                
+
+                SDL_Rect r;
+                r.x = i; r.y = j;
+                r.w = 1;
+                r.h = 1;
+
+                SDL_FillRect(s, &r, SDL_MapRGB(s->format, rgbv, rgbv, rgbv));
+
+            }
         }
-        else {
-            
-            final_intensity = 2.0 * get_color(
-                current_point.x, current_point.y,
-                p[0], p[2], p[3],
-                c[0], c[2], c[3]
-            );
 
-        }
-
-        cout << "final intensity: " << final_intensity << endl;
-
-        {
-
-            int rgbv = (final_intensity * 255);
-
-            SDL_FillRect(s, NULL, 0); // clear screen
-
-            SDL_Rect r;
-            r.x = 0; r.y = 0;
-            r.w = SCREEN_SIZE;
-            r.h = SCREEN_SIZE;
-
-            SDL_FillRect(s, &r, SDL_MapRGB(s->format, rgbv, rgbv, rgbv));
-            SDL_Flip(s);
-        }
-
+        SDL_Flip(s);
         SDL_Delay(25);
     }
 
